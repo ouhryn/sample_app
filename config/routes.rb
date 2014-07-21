0d02d177b906f13ca20e5216c3e42629a5fad429
ouhryn <ouhryn@softserveinc.com>
2014-07-21 13:03:50 -0300

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
+  resources :users
   root 'static_pages#home'
   match '/', to: 'static_pages#index', via: 'get'
   match '/help', to: 'static_pages#help', via: 'get'
