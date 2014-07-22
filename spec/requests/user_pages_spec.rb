a438716317c42f82d9eac427c26b03306dcc4e03
ouhryn <ouhryn@softserveinc.com>
2014-07-22 06:36:49 -0300

    Finish tasks for 7 chapter

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -18,8 +18,15 @@ end
 
        describe "with invalid information" do
          it "should not create a user" do
-            expect(click_button submit).not_to change(User, :count)
+            expect {click_button submit}.not_to change(User, :count)
          end
+
+           describe "after submissio" do
+             before {click_button submit}
+
+             it { should have_title('Sign up') }
+             it { should have_content('error') }
+           end
        end
 
        describe "with valid information" do
@@ -30,7 +37,15 @@ end
             fill_in "Confirmation", with: "foobar"      
           end
           it "should create a user" do
-          expect(click_button submit).to change(User, :count).by(1)
+          expect {click_button submit}.to change(User, :count).by(1)
+        end
+
+        describe "after saving the user" do
+           before { click_button submit }
+           let(:user) { User.find_by(email: 'user@example.com') }
+
+           it { should have_title(user.name) }
+           it { should have_selector('div.alert.alert-success', text: 'Welcome') }
         end
        end
     end
